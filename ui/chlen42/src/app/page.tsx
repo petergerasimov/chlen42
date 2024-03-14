7693e702cfe7d0c88dca5f61f7b41d3f945afb7a
Peter Gerasimov <petergerasimov2000@gmail.com>
2024-03-14 13:09:47 +0200

    added base ui

---
+++ ui/chlen42/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ChatPage } from "@/components/chat-page";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ChatPage></ChatPage>
+    </main>
+  );
+}
