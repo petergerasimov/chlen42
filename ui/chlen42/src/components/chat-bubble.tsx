74a9a63783692586149e958c819c105b1cf5d05e
Peter Gerasimov <petergerasimov2000@gmail.com>
2024-03-15 23:08:33 +0200

    added messages

--- ui/chlen42/src/components/chat-bubble.tsx
+++ ui/chlen42/src/components/chat-bubble.tsx
@@ -3,28 +3,21 @@ import React from "react";
 type ChatBubbleProps = {
   name: string;
   message: string;
-  left?: boolean;
+  other?: boolean;
 };
 
 function ChatBubble(props: ChatBubbleProps) {
   return (
     <div
-      className={`flex justify-${
-        props.left ? "start" : "end"
+      className={`flex flex-row justify-${
+        props.other ? "start" : "end"
       } items-start gap-4`}
     >
-      <div className="rounded-lg bg-gray-100 dark:bg-gray-800 p-4 flex items-center gap-2">
-        <img
-          alt="Avatar"
-          className="rounded-full"
-          height={40}
-          src="/placeholder.svg"
-          style={{
-            aspectRatio: "40/40",
-            objectFit: "cover",
-          }}
-          width={40}
-        />
+      <div
+        className={`rounded-lg bg-gray-100 dark:bg-gray-800 p-4 flex justify-${
+          props.other ? "start" : "end"
+        } gap-2`}
+      >
         <div className="text-sm leading-none">
           <p className="font-semibold">{props.name}</p>
           <p>{props.message}</p>
