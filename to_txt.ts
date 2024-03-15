54378c47c7ae06c663857140c361fb888e5af3f6
Peter Gerasimov <petergerasimov2000@gmail.com>
2024-03-15 10:56:13 +0200

    scraped data to txt

---
+++ to_txt.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+const data = fs.readFileSync("./data.json", "utf-8");
+const dataObj: {
+  title: string;
+  text: string;
+  article_num: string;
+}[] = JSON.parse(data);
+
+for (const chunk of dataObj) {
+  fs.writeFileSync(
+    `./data/${chunk.article_num}.txt`,
+    `${chunk.title}\n${chunk.text}`
+  );
+}
