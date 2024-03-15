bc60d0f91609bb238996899d91d83ac56c64e2de
Peter Gerasimov <petergerasimov2000@gmail.com>
2024-03-15 23:15:40 +0200

    fix message sides

--- ui/chlen42/src/components/chat-bubble.tsx
+++ ui/chlen42/src/components/chat-bubble.tsx
@@ -9,15 +9,11 @@ type ChatBubbleProps = {
 function ChatBubble(props: ChatBubbleProps) {
   return (
     <div
-      className={`flex flex-row justify-${
+      className={`flex flex-row place-content-${
         props.other ? "start" : "end"
-      } items-start gap-4`}
+      } gap-4`}
     >
-      <div
-        className={`rounded-lg bg-gray-100 dark:bg-gray-800 p-4 flex justify-${
-          props.other ? "start" : "end"
-        } gap-2`}
-      >
+      <div className="rounded-lg bg-gray-100 dark:bg-gray-800 p-4 flex gap-2">
         <div className="text-sm leading-none">
           <p className="font-semibold">{props.name}</p>
           <p>{props.message}</p>
