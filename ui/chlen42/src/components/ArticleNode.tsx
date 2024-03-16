94f016cb39f31ea1270f65955f363914917ed2ea
mdatsev <martin.datsev.p@gmail.com>
2024-03-16 05:35:39 +0200

    fixes

--- ui/chlen42/src/components/ArticleNode.tsx
+++ ui/chlen42/src/components/ArticleNode.tsx
@@ -58,7 +58,10 @@ export default function ArticleNode({ data }) {
     });
   };
 
-  const constructDataNodeComponent = (data: DataNode, depth: idFormatterKey = 0) => {
+  const constructDataNodeComponent = (
+    data: DataNode,
+    depth: idFormatterKey = 0
+  ) => {
     const idFormatter = idFormatters[depth];
     const children = getDataNodeChildren(data);
     //console.log("TONI TEST", data, data.meta);
@@ -66,17 +69,24 @@ export default function ArticleNode({ data }) {
     return (
       <div key={data.id} className="ml-4">
         <div className="relative">
-          {data.meta.dv && <FaClockRotateLeft className="text-sm text-black/50 peer absolute -left-5 top-1" />}
+          {data.meta.dv && (
+            <FaClockRotateLeft className="text-sm text-black/50 peer absolute -left-5 top-1" />
+          )}
           <b>{idFormatter(data.id)} </b>
           {data.meta.dv && (
-            <div className="absolute hidden peer-hover:block left-0 rounded p-2 bg-zinc-600 text-white">
+            <div className="absolute hidden peer-hover:block left-0 rounded p-2 bg-zinc-600 text-white z-20">
               {data.meta.dv}
             </div>
           )}
-          <span>{data.linked ? buildLinkedText(data.linked) : data.meta.text}</span>
+          <span>
+            {data.linked ? buildLinkedText(data.linked) : data.meta.text}
+          </span>
         </div>
         {children.map((child) => {
-          return constructDataNodeComponent(child, (depth + 1) as idFormatterKey);
+          return constructDataNodeComponent(
+            child,
+            (depth + 1) as idFormatterKey
+          );
         })}
       </div>
     );
@@ -92,7 +102,11 @@ export default function ArticleNode({ data }) {
         />
       </div>
       <ScrollArea className="h-full resize bg-white rounded-b-xl pl-2 nowheel nodrag text-black w-[500px] h-[250px]">
-        {data.article ? constructDataNodeComponent(data.article) : <div>Loading...</div>}
+        {data.article ? (
+          constructDataNodeComponent(data.article)
+        ) : (
+          <div>Loading...</div>
+        )}
       </ScrollArea>
       <Handle type="source" position={Position.Bottom} id="a" />
     </div>
