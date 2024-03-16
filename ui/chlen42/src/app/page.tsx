19bc5a5b278b1a4951c36fde803fdd782d471c93
Peter Gerasimov <petergerasimov2000@gmail.com>
2024-03-16 04:15:43 +0200

    added a little bit of blur

--- ui/chlen42/src/app/page.tsx
+++ ui/chlen42/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col bg-gray-100 items-center justify-between">
       <Graph></Graph>
       <div
-        className="fixed top-0 h-2/4 right-0 p-4 bg-zinc-400/80 shadow-md max-w-xl rounded-bl-xl resize"
+        className="fixed top-0 h-2/4 right-0 p-4 bg-zinc-400/80 shadow-md max-w-xl rounded-bl-xl resize backdrop-blur-sm"
         style={{
           direction: "rtl",
           overflow: "auto",
